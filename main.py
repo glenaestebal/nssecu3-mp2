c197dee6067a01cf8309f312b51edc93dd83b58b
glena estebal <eidrene_glena_estebal@dlsu.edu.ph>
2024-06-13 02:34:00 +0800

    added display

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import subprocess # this module will execute a child program in a new process
 import os
 
-RBCmd_process = subprocess.Popen(
-    ["RBCmd.exe"],
-    shell=True
-)
+def display():
+    print( " _____  ____   _____               _           _____                     _   ______ _ _       _____           _          _____                         ")
+    print("|  __ \|  _ \ / ____|             | |    _    |  __ \                   | | |  ____(_) |     / ____|         | |        |  __ \                        ")
+    print("| |__) | |_) | |     _ __ ___   __| |  _| |_  | |__) |___  ___ ___ _ __ | |_| |__   _| | ___| |     __ _  ___| |__   ___| |__) |_ _ _ __ ___  ___ _ __ ")
+    print("|  _  /|  _ <| |    | '_ ` _ \ / _` | |_   _| |  _  // _ \/ __/ _ \ '_ \| __|  __| | | |/ _ \ |    / _` |/ __| '_ \ / _ \  ___/ _` | '__/ __|/ _ \ '__|")
+    print("| | \ \| |_) | |____| | | | | | (_| |   |_|   | | \ \  __/ (_|  __/ | | | |_| |    | | |  __/ |___| (_| | (__| | | |  __/ |  | (_| | |  \__ \  __/ |   ")
+    print("|_|  \_\____/ \_____|_| |_| |_|\__,_|         |_|  \_\___|\___\___|_| |_|\__|_|    |_|_|\___|\_____\__,_|\___|_| |_|\___|_|   \__,_|_|  |___/\___|_|   ")
 
-# # Communicate with the process (send input and receive output)
-# stdout, stderr = RBCmd.communicate()
+# def main_menu(choice):
 
-# # print the output
-# print("Output:", stdout)
-# print("Errors:", stderr)
 
-# Check the return code
-if RBCmd_process.returncode != 0:
-    print(f"Command failed with return code {RBCmd_process.returncode}")
+# RBCmd_process = subprocess.Popen(
+#     ["RBCmd.exe"],
+#     shell=True
+# )
+
+if __name__ == "__main__":
+    display()
+
+# # Check the return code
+# if RBCmd_process.returncode != 0:
+#     print(f"Command failed with return code {RBCmd_process.returncode}")
 
 
 
