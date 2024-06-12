1e0bc6fc5755cd3b669e9d3ea8d643bdd407d909
glena estebal <eidrene_glena_estebal@dlsu.edu.ph>
2024-06-13 04:03:15 +0800

    tried something new: one process function for both tools

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess   #this module will execute a child program in a new process
 
 
+
 def display():
     print(" ___ ___  ___          _     _     ___                _   ___ _ _      ___         _        ___                      ")
     print("| _ \ _ )/ __|_ __  __| |  _| |_  | _ \___ __ ___ _ _| |_| __(_) |___ / __|__ _ __| |_  ___| _ \__ _ _ _ ___ ___ _ _ ")
@@ -10,32 +11,48 @@ def display():
     print("RecentFileCacheParser parses RecentFileCacheParser.bcf files.")
     print("This tool will help analyze the user's deleted files and recently accessed files.\n")
     
+def help():
 
-def run_RBCmd():
-    RBCmd_process = subprocess.Popen(
-        ["RBCmd.exe"],
+    print("-d       directory to process")
+    print("-f       file to process")
+    print("-q       only show the filename being processed")
+    print("--csv    directory to save CSV formatted results")
+    
+# def run_RBCmd():
+#     RBCmd_process = subprocess.Popen(
+#         ["RBCmd.exe"],
+#         shell=True
+#     )
+
+# def run_RecentFileCacheParser():
+#     RecentFileCacheParser_process = subprocess.Popen(
+#         ["RecentFileCacheParser.exe"],
+#         shell=True
+#     )
+
+def run_executable(executable_name, args):
+    process = subprocess.Popen(
+        [executable_name] + args,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
         shell=True
     )
 
+    stdout, stderr = process.communicate()
 
-def run_RecentFileCacheParser():
-    RecentFileCacheParser_process = subprocess.Popen(
-        ["RecentFileCacheParser.exe"],
-        shell=True
-    )
 
 def main():
+    display()
     RBCmd_output = run_RBCmd()
     RecentFileCacheParser_output = run_RecentFileCacheParser()
 
-    with open(output_path, 'w') as file:
-        file.write(RBCmd_output)
+    # with open(output_path, 'w') as file:
+    #     file.write(RBCmd_output)
 
-    with open(output_path, 'w') as file:
-        file.write(RecentFileCacheParser_output)
+    # with open(output_path, 'w') as file:
+    #     file.write(RecentFileCacheParser_output)
 
 if __name__ == "__main__":
-    display()
     main()
 
 # # Check the return code
