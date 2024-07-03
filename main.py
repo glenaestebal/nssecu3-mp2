d1fe3e951631bc77f2d2f91cc0a2ab3c93693d48
Glena Estebal <eidrene_glena_estebal@dlsu.edu.ph>
2024-07-04 01:50:14 +0800

    fixed error in RBCmd

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import shutil
 
 def display():
     print(" ___ ___  ___          _     _     ___                _   ___ _ _      ___         _        ___                      ")
-    print("| _ \ _ )/ __|_ __  __| |  _| |_  | _ \___ __ ___ _ _| |_| __(_) |___ / __|__ _ __| |_  ___| _ \__ _ _ _ ___ ___ _ _ ")
-    print("|   / _ \ (__| '  \/ _` | |_   _| |   / -_) _/ -_) ' \  _| _|| | / -_) (__/ _` / _| ' \/ -_)  _/ _` | '_(_-</ -_) '_|")
-    print("|_|_\___/\___|_|_|_\__,_|   |_|   |_|_\___\__\___|_||_\__|_| |_|_\___|\___\__,_\__|_||_\___|_| \__,_|_| /__/\___|_|  ")
+    print("| _ \\ _ )/ __|_ __  __| |  _| |_  | _ \\___ __ ___ _ _| |_| __(_) |___ / __|__ _ __| |_  ___| _ \\__ _ _ _ ___ ___ _ _ ")
+    print("|   / _ \\ (__| '  \\/ _` | |_   _| |   / -_) _/ -_) ' \\  _| _|| | / -_) (__/ _` / _| ' \\/ -_)  _/ _` | '_(_-</ -_) '_|")
+    print("|_|_\\___/\\___|_|_|_\\__,_|   |_|   |_|_\\___\\__\\___|_||_\\__|_| |_|_\\___|\\___\\__,_\\__|_||_\\___|_| \\__,_|_| /__/\\___|_|  ")
     print("\nRBCmd is Windows Recycle Bin artifact parser.")
     print("RecentFileCacheParser parses RecentFileCacheParser.bcf files.")
     print("This tool will help analyze the user's deleted files and recently accessed files.\n")
@@ -19,23 +19,16 @@ def run_RBCmd(output_folder_path):
     print("Running RBCmd.exe...")
 
     if os.path.exists(output_folder_path):
-        try:
-            shutil.rmtree(output_folder_path)
-            print(f"Existing folder removed: {output_folder_path}")
-
-        except Exception as e:
-            print(f"Error removing existing folder: {e}")
+        shutil.rmtree(output_folder_path)
+        print(f"Existing folder removed: {output_folder_path}")
 
     try:
-        
-        subprocess.run(
-            ["mkdir", output_folder_path],
-            shell=True,
-            check=True
-        )
 
+        os.makedirs(output_folder_path)
+        print(f"Directory created: {output_folder_path}")
         RBCmd_process = subprocess.Popen(
-            ["RBCmd.exe", "-d", "C:\$Recycle.Bin","--csv", output_folder_path],
+            ["RBCmd.exe", "-d", "C:\\$Recycle.Bin","--csv", output_folder_path],
+            # stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
@@ -45,9 +38,10 @@ def run_RBCmd(output_folder_path):
         RBCmd_process.communicate()
 
         if RBCmd_process.returncode != 0:
-            print(f"Command failed with error: {RBCmd_process.stderr.read()}")
+            print(f"Command failed with error")
     
-        print(f"Output saved to {output_folder_path}.")
+        else:
+            print(f"Output saved to {output_folder_path}.")
         
     except Exception as e:
         print("Error:", e)
